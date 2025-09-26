f46fd8d428e621985c3c13d887f6742224a38a46
Yachna Raghav <142567363+Yachna63893@users.noreply.github.com>
2025-09-27 03:00:50 +0530

    Update Array_From_Permutation.java

--- Day1/Array_From_Permutation.java
+++ Day1/Array_From_Permutation.java
@@ -1,33 +1,3 @@
-// package Leetcode;
-
-// import java.util.Scanner;
-
-// public class Array_From_Permutation {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-//         int[] arr = new int[n];
-//         for(int i=0; i<arr.length; i++){
-//             arr[i] = sc.nextInt();
-//         }
-//         Create(arr);
-//         sc.close();
-//     }
-//     public static void Create(int[] arr){
-//         int n = arr.length;
-//         int[] ans = new int[n];
-//         for(int i=0; i<arr.length; i++){
-//             ans[i] = arr[arr[i]];
-//         }
-//         for(int i=0; i<arr.length; i++){
-//             System.out.print(ans[i]+" ");
-//         }
-//     }
-// }
-
-
-
-
 package ICP.Day1;
 
 import java.util.Scanner;
@@ -55,4 +25,5 @@ public class Array_From_Permutation {
         }
         return ans;
     }
+
 }
