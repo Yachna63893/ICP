9c8d9e74de6cc85fe9efa84e3b3f781950c43659
Yachna Raghav <142567363+Yachna63893@users.noreply.github.com>
2025-10-08 00:00:05 +0530

    Done

---
+++ Day9/Palindrome_Linked_List.java
@@ -0,0 +1,36 @@
+package ICP.Day9;
+
+import java.util.*;
+
+public class Palindrome_Linked_List {
+
+    public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+    class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while (temp != null) {
+            list.add(temp.val);
+            temp = temp.next;
+        }
+
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            if (!list.get(i).equals(list.get(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
+}
